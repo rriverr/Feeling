5995a0043b600f1f1470c38cc1de380329224c7c
yuna seo <playamongthestars000@gmail.com>
2022-03-08 10:25:13 +0900

    로그인

---
+++ app.py
@@ -0,0 +1,95 @@
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+
+app = Flask(__name__)
+import jwt
+import hashlib
+import datetime
+from datetime import datetime, timedelta
+from pymongo import MongoClient
+
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
+
+SECRET_KEY = 'SPARTA'
+
+client = MongoClient('mongodb+srv://test:yunayuna@cluster0.5i0os.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.dbsparta_plus_week4
+
+
+@app.route('/')
+def home():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.feelingusers.find_one({"userid": payload["id"]})
+        return render_template('index.html', user_info=user_info)
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+
+
+@app.route('/login')
+def login():
+    msg = request.args.get("msg")
+    return render_template('index.html', msg=msg)
+
+
+@app.route('/sign_up/check_dup', methods=['POST'])
+def check_dup():
+    userid_receive = request.form['userid_give']
+    exists = bool(db.feelingusers.find_one({"userid": userid_receive}))
+    return jsonify({'result': 'success', 'exists': exists})
+
+
+@app.route('/sign_up/save', methods=['POST'])
+def sign_up():
+    userid_receive = request.form['userid_give']
+    password_receive = request.form['password_give']
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    doc = {
+        "userid": userid_receive,
+        "password": password_hash,
+        "profile_name": userid_receive
+    }
+    db.feelingusers.insert_one(doc)
+    return jsonify({'result': 'success'})
+
+
+@app.route('/sign_in', methods=['POST'])
+def sign_in():
+    userid_receive = request.form['userid_give']
+    password_receive = request.form['password_give']
+
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    result = db.feelingusers.find_one({'userid': userid_receive, 'password': pw_hash})
+
+    if result is not None:
+        payload = {
+            'id': userid_receive,
+            'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
+
+        return jsonify({'result': 'success', 'token': token})
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
+
+@app.route('/user/<userid>')
+def user(userid):
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        status = (userid == payload["id"])
+
+        user_info = db.feelingusers.find_one({"userid": userid}, {"_id": False})
+        return render_template('user.html', user_info=user_info, status=status)
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
